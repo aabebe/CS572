0a06b78ef99c6410bce65d56ced9b120c3bc31fa
aabebe <aabebe@mum.edu>
2019-01-28 21:57:45 -0600

    instantiating the output on app.component

--- homework_11/exercise1/src/app/app.component.ts
+++ homework_11/exercise1/src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-counter (counterChange) = "showoutput($event)" > </app-counter>
+  template: `<app-counter (counterChange) = 'showoutput($event)' > </app-counter>
 <p> Component Counter Value= {{counter}}</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   @Input() counter: number;
   title = 'exercise1';
+  constructor() {
+    this.counter = 0;
+  }
   showoutput(data) {
     console.log(data);
     this.counter = data;
